47eb46666359b8c361db8ed0055988b0b7d8f332
Sota Nishiyama <2480souta@gmail.com>
2017-07-06 15:30:45 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,374 @@
+/**
+last modified: 2016/10/31
+author: Sota Nishiyama
+**/
+
+// global
+var screen;
+var ctx;
+
+var bar;
+var ball;
+var blocks;
+var items;
+
+var fire = false;
+var breakAll = false;
+
+var mouse = new Point;
+var ballVector = new Point();
+
+var count = 0;
+var itemCount = 0;
+var level = 0;
+var score = 0;
+
+var i,j;
+
+var itemSound = new Audio("item.mp3");
+
+//const
+const FPS = 1000 / 50;
+const INFO_HEIGHT = 30;
+const ITEM_FREQUENCY = 10;    // the greater the rarer
+const MSG_COLOR = '#FFAD80';
+
+// event
+function mouseMove () {
+	// update the coordinate of the mouse cursor
+	mouse.x = event.clientX - screen.offsetLeft;
+}
+
+function mouseDown () {
+	if (level === 0) { // start the game
+		level = 1;
+		count = 0;
+	} else { // launch the ball
+		fire = true
+	}
+}
+
+window.onload = function () {
+	screen = document.getElementById('screen');
+	screen.width = 500;
+	screen.height = 600;
+
+	ctx = screen.getContext('2d');
+
+	screen.addEventListener('mousemove', mouseMove, true);
+	screen.addEventListener('mousedown', mouseDown, true);
+
+	//initialization
+	bar = new Bar();
+	bar.init();
+	bar.position.y = 550;
+	mouse.x = screen.width/2;    // initial position of the bar
+
+	ball = new Ball();
+	ball.init();
+	ballVector.y = -bar.width / 2;
+
+	var rowNumber = blockArrange[0][0].length;
+	var interval = (screen.width - rowNumber * BLOCK_WIDTH) / (rowNumber + 1);
+	blocks = new Array(blockArrange[0].length);
+
+	var row, col;
+	for (row = 0; row < blocks.length; row++) {
+		blocks[row] = new Array(rowNumber);
+
+		for (col = 0; col < rowNumber; col++) {
+			blocks[row][col] = new Block();
+
+			var blockPoint = new Point();
+			blockPoint.x = BLOCK_WIDTH * (col + 0.5) + interval * (col + 1);
+			blockPoint.y = BLOCK_HEIGHT * (row + 0.5) + interval * (row + 1) + INFO_HEIGHT;
+
+			blocks[row][col].init(blockPoint);
+		}
+	}
+
+	items = new Array();
+	var index = 0;
+
+	// call loop処理
+	(function () {
+
+		count++;
+		itemCount++;
+
+		ctx.clearRect(0, 0, screen.width, screen.height);
+		ctx.beginPath();
+		ctx.fillStyle = '#909090';
+		ctx.fillRect(0, INFO_HEIGHT, screen.width, 3);
+
+		// calculate the positon of the bar
+		var half = bar.width / 2
+		if (mouse.x < half) {
+			bar.position.x = half;
+		} else if (mouse.x > screen.width - half) {
+			bar.position.x = screen.width - half;
+		} else {
+			bar.position.x = mouse.x;
+		}
+
+		// draw the bar
+		ctx.beginPath();
+		ctx.fillStyle = bar.color;
+		ctx.fillRect(bar.position.x - half, bar.position.y - (bar.height / 2), bar.width, bar.height);
+
+		//レベルで分岐
+		if (level === 0) {
+
+			// title
+			ctx.beginPath();
+			ctx.font = '100px Consolas';
+			ctx.fillStyle = MSG_COLOR;
+			ctx.textAlign = 'center';
+			ctx.fillText('BLOCK', screen.width / 2, screen.height / 2 - 100);
+			ctx.fillText('BREAKING', screen.width / 2, screen.height / 2);
+			ctx.font = '20px Consolas';
+			ctx.fillText('Click the screen to start the game', screen.width / 2, screen.height / 2 + 100);
+
+			// ready for level 1
+			for (i = 0; i < blocks.length; i++) {
+				for (j = 0; j < blocks[i].length; j++) {
+					blocks[i][j].set(1, i, j);
+				}
+			}
+
+		} else {
+
+			// move the ball
+			if (!fire) {
+
+				// set the ball
+				var ballPoint = new Point;
+				ballPoint.x = bar.position.x;
+				ballPoint.y = bar.position.y - (bar.height / 2) - ball.size;
+				ballVector.x = 0;
+				ball.set(ballPoint, ballVector);
+
+			} else {
+
+				//move the ball
+				ball.move();
+
+				//ボール・バー衝突判定--------------------
+				var place = judge(ball,bar);
+
+				switch (place) {
+
+					case 'upper':
+					case 'lower':
+					case 'left':
+					case 'right':
+
+					ballVector.x = ball.position.x - bar.position.x;
+					ballVector.y = - bar.width / 2;
+					ball.set(ball.position, ballVector);
+
+				}
+
+				// ball, block衝突判定--------------------
+				breakAll = true;
+
+				for (i = 0; i < blocks.length; i++) {
+					for (j = 0; j < blocks[i].length; j++) {
+
+						if (blocks[i][j].alive) {
+							place = judge(ball, blocks[i][j]);
+
+							switch (place) {
+								case 'upper':
+								case 'lower':
+								blocks[i][j].life--;
+								if (!ball.penetration || blocks[i][j].life !== 0) {
+									ball.vector.y *= -1;
+								}
+								break;
+
+								case 'left':
+								case 'right':
+								blocks[i][j].life--;
+								if (!ball.penetration || blocks[i][j].life !== 0) {
+									ball.vector.x *= -1;
+								}
+								break;
+							}
+
+							// when a block gets broken
+							if (blocks[i][j].life === 0) {
+								blocks[i][j].alive = false;
+								score += blocks[i][j].score;
+								// create an item
+								var makeItem = Math.floor(Math.random() * ITEM_FREQUENCY);
+								if (makeItem === 0) {
+									items[index] = new Item();
+									items[index].init(blocks[i][j].position);
+									index++;
+								}
+							}
+
+							breakAll=false;
+						}
+					}
+				}
+
+			}
+
+			// draw blocks
+			ctx.beginPath();
+
+			for (i = 0; i < blocks.length; i++) {
+				for ( j = 0; j < blocks[i].length; j++) {
+					if (blocks[i][j].alive) {
+						ctx.fillStyle = blocks[i][j].color;
+						ctx.fillRect(
+							blocks[i][j].position.x - blocks[i][j].width / 2,
+							blocks[i][j].position.y - blocks[i][j].height / 2,
+							blocks[i][j].width,
+							blocks[i][j].height
+							);
+						ctx.closePath();
+					}
+				}
+			}
+
+			// items
+			for ( i = 0; i < index; i++) {
+				if (items[i].alive) {
+
+					items[i].move();
+
+					place=judge(items[i],bar);
+					var itemNum = Math.floor(Math.random() * itemTypes.length);
+
+					switch(place){
+						case 'upper':
+						case 'lower':
+						case 'left':
+						case 'right':
+
+						items[i].alive = false;
+						bar.init();
+						ball.init();
+						itemTypes[itemNum].apply(bar, ball);
+						itemCount = 0;
+						itemSound.play();
+					}
+
+					// draw
+					ctx.beginPath();
+					// set the gradation
+					var gradation = ctx.createLinearGradient(
+						items[i].position.x - items[i].size,
+						items[i].position.y - items[i].size,
+						items[i].position.x - items[i].size,
+						items[i].position.y + items[i].size
+						);
+					gradation.addColorStop(0, '#00FF60'); 
+ 					gradation.addColorStop(1, '#00A8FF');
+
+					ctx.fillStyle = gradation;
+					ctx.arc(items[i].position.x, items[i].position.y, items[i].size, 0, Math.PI * 2, false);
+					ctx.fill();
+
+					ctx.font='20px Consolas';
+					ctx.fillStyle='#404040';
+					ctx.textAlign='center';
+					ctx.textBaseline='middle';
+					ctx.fillText('?', items[i].position.x, items[i].position.y);
+				}
+			}
+
+			// draw the ball
+			ctx.beginPath();
+			ctx.arc(ball.position.x, ball.position.y, ball.size, 0, Math.PI * 2, false);
+			ctx.fillStyle = ball.color;
+			ctx.fill();
+
+			//一定時間レベル表示
+			if (count < (1000 / FPS) * 1) { // 1 second
+				ctx.beginPath();
+				ctx.font = '50px Consolas';
+				ctx.fillStyle = MSG_COLOR;
+				ctx.textAlign = 'center';
+				ctx.fillText('level ' + level, screen.width / 2, screen.height / 2);
+			}
+
+			//一定時間で初期化
+			if (itemCount === (1000 / FPS) * 10) { // 10 seconds
+				bar.init();
+				ball.init();
+			}
+
+			//ライフ、レベル、スコア表示
+			//ライフ
+			for (i = 1; i < ball.life; i++) {
+				ctx.beginPath();
+				ctx.arc(20 * i,INFO_HEIGHT / 2, 8, 0, Math.PI * 2, false);
+				ctx.fillStyle = ball.color;
+				ctx.fill();
+			}
+			//レベル、スコア
+			ctx.beginPath();
+			ctx.font = '20px Consolas';
+			ctx.fillStyle = MSG_COLOR;
+			ctx.textAlign = 'center';
+			ctx.textBaseline = 'middle';
+			var msg = 'level ' + level + '     ' + 'score ' + score;
+			ctx.fillText(msg, screen.width / 2, INFO_HEIGHT / 2);
+
+			// when clear
+			if (breakAll) {
+				// ready for the next level
+				level++;
+				count = 0;
+				breakAll = false;
+				bar.init();
+				ball.init();
+
+				for (i = 0; i < index; i++) {
+					items[i].alive = false;
+				}
+
+				// finish the game
+				if (level - 1 === blockArrange.length) {
+					ctx.beginPath();
+					ctx.font = '100px Consolas';
+					ctx.fillStyle = MSG_COLOR;
+					ctx.textAlign = 'center';
+					ctx.fillText('CLEAR!', screen.width / 2, screen.height / 2);
+					ctx.font = '50px Consolas';
+					ctx.fillText('score: ' + score, screen.width / 2, screen.height / 2 + 100);
+
+					return;
+				} else {
+					fire = false;
+
+					for (i = 0; i < blocks.length; i++) {
+						for (j = 0; j < blocks[i].length; j++) {
+							blocks[i][j].set(level, i, j);
+						}
+					}
+				}
+			}
+
+			// game over
+			if (ball.life === 0) {
+				ctx.beginPath();
+				ctx.font = '100px Consolas';
+				ctx.fillStyle = MSG_COLOR;
+				ctx.textAlign = 'center';
+				ctx.fillText('GAME', screen.width / 2, screen.height / 2 - 50);
+				ctx.fillText('OVER', screen.width / 2, screen.height / 2 + 50);
+				ctx.font = '50px Consolas';
+				ctx.fillText('score: ' + score, screen.width / 2, screen.height / 2 + 150);
+
+				return;
+			}
+		}
+
+		setTimeout(arguments.callee, FPS);
+	}) ();
+}
---
+++ objects.js
@@ -0,0 +1,295 @@
+const BAR_WIDTH  = 160;
+const BAR_HEIGHT = 15;
+const BAR_COLOR  = '#808080';
+
+function Bar() {
+	this.position = new Point();
+	this.width = 0;
+	this.height = 0;
+	this.color = '';
+	this.shape = 'rect';
+}
+
+Bar.prototype.init = function () {
+	this.width = BAR_WIDTH;
+	this.height = BAR_HEIGHT;
+	this.color = BAR_COLOR;
+};
+
+const BALL_SIZE  = 10;
+const BALL_SPEED = 10;
+const BALL_COLOR = '#808080';
+
+function Ball() {
+	this.position = new Point();
+	this.vector = new Point();
+	this.size = 0;
+	this.speed = 0;
+	this.life = 5;
+	this.penetration = false;
+	this.color = '';
+	this.shape = 'circle';
+}
+
+Ball.prototype.init = function () {
+	this.size = BALL_SIZE;
+	this.speed = BALL_SPEED;
+	this.color = BALL_COLOR;
+	this.penetration = false;
+}
+
+Ball.prototype.set = function (position, vector) {
+	this.position.x = position.x;
+	this.position.y = position.y;
+	vector.normalize();
+	this.vector.x = vector.x;
+	this.vector.y = vector.y;
+};
+
+Ball.prototype.move = function () {
+	//座標をベクトルに応じてspeed分だけ移動させる
+	this.position.x += this.vector.x * this.speed;
+	this.position.y += this.vector.y * this.speed;
+
+	//壁に当たったら向きを変える
+	if ((this.position.x - this.size < 0 && this.vector.x < 0) || (this.position.x + this.size > screen.width && this.vector.x > 0)) {
+		this.vector.x *= -1;
+	}
+	if (this.position.y  -this.size < INFO_HEIGHT - 3 && this.vector.y < 0) {
+		this.vector.y *= -1;
+	}
+
+	//一定以上の座標に到達していたら生存フラグを降ろす
+	if (this.position.y - this.size > screen.height) {
+		fire = false;
+		this.life--;
+		bar.init(BAR_WIDTH, BAR_HEIGHT, BAR_COLOR);
+		ball.init(BALL_SIZE, BALL_SPEED, BALL_COLOR);
+		for (i = 0; i < items.length; i++) {
+			items[i].alive = false;
+		}
+	}
+	
+};
+
+//block
+var blockArrange = [
+	[
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[1,1,1,1,1,1,1,1,1,1,1,1,1],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[1,1,1,1,1,1,1,1,1,1,1,1,1],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0]
+	],
+	// [
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	// 	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	// 	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	// 	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	// 	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	// 	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0]
+	// ],
+	// [
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	// 	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	// 	[3,3,3,3,3,3,3,3,3,3,3,3,3],
+	// 	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	// 	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	// 	[3,3,3,3,3,3,3,3,3,3,3,3,3],
+	// 	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	// 	[0,0,0,0,0,0,0,0,0,0,0,0,0]
+	// ],
+	[
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[2,1,2,1,2,1,2,1,2,1,2,1,2],
+		[2,3,2,3,2,3,2,3,2,3,2,3,2],
+		[1,1,1,1,1,1,1,1,1,1,1,1,1],
+		[2,2,2,2,2,2,0,2,2,2,2,2,2],
+		[0,3,0,3,0,3,0,3,0,3,0,3,0],
+		[1,1,1,1,1,1,0,1,1,1,1,1,1],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0]
+	],
+]
+
+const BLOCK_WIDTH  = 37;
+const BLOCK_HEIGHT = 20;
+
+function Block() {
+	this.position = new Point();
+	this.width = 0;
+	this.height = 0;
+	this.score = 0;
+	this.life = 5;
+	this.color = '';
+	this.alive = false;
+	this.shape = 'rect';
+}
+
+Block.prototype.init = function (position) {
+	this.position.x = position.x;
+	this.position.y = position.y;
+	this.width = BLOCK_WIDTH;
+	this.height = BLOCK_HEIGHT;
+}
+
+Block.prototype.set = function (level, row, col) {
+	level -= 1;
+	if (blockArrange[level][row][col] !== 0) {
+		this.alive = true;
+		this.life = blockArrange[level][row][col];
+		this.score = blockArrange[level][row][col] * 10;
+		switch (blockArrange[level][row][col]) {
+			case 1:
+			this.color = '#02CBFD';
+			break;
+			case 2:
+			this.color = '#02FD0E'
+			break;
+			case 3:
+			this.color = '#DEFF00';
+			break;
+		}
+	} else {
+		this.alive = false;
+	}
+}
+
+// item
+var itemTypes = [
+	{
+		name: 'bigBar',
+		apply: function (bar, ball) {
+			bar.width = BAR_WIDTH * 1.5;
+			bar.color = '#FF4545';
+		}
+	},
+	{
+		name: 'smallBar',
+		apply: function (bar, ball) {
+			bar.width = BAR_WIDTH / 2;
+			bar.color = '#477BFF';
+		}
+	},
+	{
+		name: 'fastBall',
+		apply: function (bar, ball) {
+			ball.speed = BALL_SPEED * 1.5;
+			ball.color = '#FC0101';
+		}
+	},
+	{
+		name: 'slowBall',
+		apply: function (bar, ball) {
+			ball.speed = BALL_SPEED / 2;
+			ball.color = '#0802FE';
+		}
+	},
+	{
+		name: 'bigBall',
+		apply: function (bar, ball) {
+			ball.size = BALL_SIZE * 1.5;
+			ball.color = '#FF4545';
+		}
+	},
+	{
+		name: 'smallBall',
+		apply: function (bar, ball) {
+			ball.size = BALL_SIZE / 2;
+			ball.color = '#477BFF';
+		}
+	},
+	{
+		name: 'penetration',
+		apply: function (bar, ball) {
+			ball.color = '#FF8400';
+			ball.penetration = true;
+		}
+	}
+];
+
+const ITEM_SIZE = 10;
+const ITEM_SPEED = 3;
+
+function Item() {
+	this.position = new Point();
+	this.size = 0;
+	this.speed = 0;
+	this.kind = '';
+	this.alive = false;
+	this.shape = 'circle';
+}
+
+Item.prototype.init = function (position) {
+	this.position.x = position.x;
+	this.position.y = position.y;
+	this.size = ITEM_SIZE;
+	this.speed = ITEM_SPEED;
+	this.alive = true;
+}
+
+Item.prototype.move = function () {
+	this.position.y += this.speed;
+
+	// bottom
+	if(this.position.y - this.size > screen.height) {
+		this.alive = false;
+	}
+}
+
+/*
+judge(obj1, obj2)
+obj1 ... Ball or Item
+obj2 ... Bar or Block
+*/
+function judge (obj1, obj2) {
+	var differenceX = obj2.position.x - obj1.position.x;
+	var differenceY = obj2.position.y - obj1.position.y;
+
+	if ( // top and bottom
+		Math.abs(differenceX) <= obj2.width / 2
+		) {
+		if ( // top
+			differenceY <= obj2.height / 2 + obj1.size &&
+			differenceY >= 0
+			) {
+			return 'upper';
+
+		} else if ( // bottom
+			- differenceY <= obj2.height / 2 + obj1.size &&
+			- differenceY >= 0
+			) {
+			return 'lower';
+		}
+
+	} else if ( // side
+		Math.abs(differenceY) <= obj2.height / 2
+		) {
+
+		if ( // left
+			differenceX <= obj2.width / 2 + obj1.size &&
+			differenceX >= 0
+			) {
+			return 'left';
+
+		} else if ( // right
+			- differenceX <= obj2.width / 2 + obj1.size &&
+			- differenceX >= 0
+			) {
+			return 'right';
+		}
+	}
+	return false;
+}
